ab52c1f567b3fd300ac8ba68025d1e4cc6b62a0c
hychoi <hayeong28@naver.com>
2020-04-03 04:20:41 +0900

    Updates

---
+++ precache-manifest.8ac88d39026bb8a1a352cd0cfd94f15a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913ec9d5aeaa37943514cd36838746d2",
+    "url": "/movie_app_hychoi/index.html"
+  },
+  {
+    "revision": "d7876724a1be90a03948",
+    "url": "/movie_app_hychoi/static/css/main.8ec89522.chunk.css"
+  },
+  {
+    "revision": "f1cb88563a1fd6f90d32",
+    "url": "/movie_app_hychoi/static/js/2.a6c4b584.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_hychoi/static/js/2.a6c4b584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7876724a1be90a03948",
+    "url": "/movie_app_hychoi/static/js/main.1bc788e9.chunk.js"
+  },
+  {
+    "revision": "ea737abf67e93d2ac39d",
+    "url": "/movie_app_hychoi/static/js/runtime-main.4b409c04.js"
+  }
+]);
